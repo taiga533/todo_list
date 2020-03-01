cef04c9cf40cccf0b6a8148fe1cd4c7ee14c9bd9
junyades <junyaadgj@gmail.com>
2020-03-01 15:00:13 +0900

    チェックリストの棒線いじった

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 let completedTodo = document.querySelector(".completed")
-completedTodo.classList.remove("completed")
+let check = document.querySelector(".toggle")
+console.log(check)
+check.onclick = function(){
+    if(check.checked === false){
+        completedTodo.classList.remove("completed")
+    }else{
+        completedTodo.classList.add("completed")
+    }
+}
