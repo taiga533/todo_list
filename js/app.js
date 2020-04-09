e9482207623a1dad74b38bbc11fad95f7a5be644
内園潤也 <uchizonojunya@uchienuruunarinoMacBook-Pro.local>
2020-04-09 23:30:00 +0900

    完了、削除、リファクタリング

--- js/app.js
+++ js/app.js
@@ -1,39 +1,33 @@
-const completedTodo = document.querySelector(".completed")
-const check = document.querySelector(".toggle")
-
-console.log(check)
-check.onclick = function(){
-    if(check.checked === false){
-        completedTodo.classList.remove("completed")
-        
-    }else{
-        completedTodo.classList.add("completed")
-        
-    }
-}
 //4月8日からやり始めた範囲↓
-const comp = document.querySelector("#comp")
-const okashi = document.querySelector("#okashi")
 
-okashi.addEventListener('click',()=>{
-    if(okashi.checked === true){
-        comp.classList.add("completed");
-    }else{
-        comp.classList.remove("completed");
-    }
-})
-//お菓子にチェックを付けられるようにした↑
+const initalizeToggleCheckboxes = () => {
+    const toggleCheckboxes = document.querySelectorAll(".toggle")
 
-const okashiDel = document.querySelector("#okashiDel");
-okashiDel.addEventListener('click',()=>{
-    comp.remove();
-})
-//バツボタンでお菓子欄削除↑
+    toggleCheckboxes.forEach(toggleCheckbox =>{
+        toggleCheckbox.addEventListener('click',()=>{
+            if(toggleCheckbox.checked === true){
+                toggleCheckbox.parentNode.parentNode.classList.add("completed")
+            }else{
+                toggleCheckbox.parentNode.parentNode.classList.remove("completed")
+            }
+        })
+    })
+}
 
-const ramenDel = document.querySelector("#ramenDel");
-ramenDel.addEventListener('click',()=>{
-    completedTodo.remove();
-})
+const initalizeDestroyButton = () => {
+    const todoWrapper = document.querySelector("#todoList")
+    const destroyButtons = document.querySelectorAll(".destroy")
+
+    destroyButtons.forEach(destroyButton => {
+        destroyButton.addEventListener('click',() => {
+            todoWrapper.removeChild(destroyButton.parentNode.parentNode)
+        })
+    });
+}
+
+// 初期化処理
+initalizeDestroyButton()
+initalizeToggleCheckboxes()
 
 
 
